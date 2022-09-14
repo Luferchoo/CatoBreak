6da2da66205446f5b2fa2036de6d12a51e6e8ad2
lufercho <luisfernandogilgareca@gmail.com>
2022-09-14 14:20:03 -0400

    Main: Database connected

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const mongoose = require('mongoose');
 
 require('dotenv').config();
 
@@ -25,6 +26,18 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+mongoose.connect(process.env.CONNECTION_STRING, {useNewUrlParser: true, UseUnifiedTopology: true});
+
+const connection = mongoose.connection;
+
+connection.on('error', () =>{
+  console.log('Error connection to database');
+});
+
+connection.once('open', () =>{
+  console.log('Connected to Database.....');
+});
 
 app.use('/', indexRouter);
 /*app.use('/auth', authRouter);
