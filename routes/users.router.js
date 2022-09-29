27db4a106d27e354f465b805e4af6f5a8a1fbc03
sebasArroyo2408 <sebasarroyoBerzain@gmail.com>
2022-09-29 13:22:14 -0400

    backend: last changes in backend, methods get and post implemented in classes user, lunches and games

--- routes/users.router.js
+++ routes/users.router.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var router = express.Router();
+const createError = require('http-errors');
+const {jsonResponse}= require('../jsonresponse');
+
 const User = require('../model/users.model');
 
 
@@ -13,6 +16,28 @@ router.get('/', async function(req, res, next) {
   }
 
   res.json(results)
+});
+
+router.post('/', async function(req,res, next){
+  //console.log("XD")
+  const {username, password}= req.body;
+
+  if(!username || !password){
+    next(createError(400, 'Username and/or password missing'));
+  }else if(username && password){
+    const user= new User({username, password});
+
+    const exists = await user.usernameExists(username);
+
+    if(exists){
+      next(createError(400, 'User name is taken. Try again'));
+    }else{
+      await user.save();
+      res.json(jsonResponse(200, {
+        message: 'User created successfully'
+      }));
+    }
+  }
 });
 
 module.exports = router;
