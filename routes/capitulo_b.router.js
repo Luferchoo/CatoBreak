5f747d88a8fe7f6d5f3881cc0e76d5c05a4fb88b
sebasArroyo2408 <sebasarroyoBerzain@gmail.com>
2022-11-22 10:13:58 -0400

    Cap B_ Get and Post fixed

--- routes/capitulo_b.router.js
+++ routes/capitulo_b.router.js
@@ -7,17 +7,15 @@ const capitulo_b = require('../model/capitulo_b.model');
 console.log(capitulo_b)
 
 router.get('/',async (req, res, next)=>{
-    let results={};
     try{
-        results = await capitulo_b.find({});
-        console.log(results)
-    } catch(ex){
-        next(createError(500,'Error fetching results'))
+        const capitulo_b = await capitulo_b.find()
+        res.status(200).json(tests)
+    } catch(err){
+        res.status(500).json({message: err.message })
     }
-    res.json(results)
 });
 
-router.post('/',async (req, res, next)=>{
+router.post('/',async (req, res)=>{
     const cap_b = new capitulo_b({
         cap_b_preg_1:req.body.cap_b_preg_1,
         cap_b_preg_2:req.body.cap_b_preg_2,
@@ -39,23 +37,15 @@ router.post('/',async (req, res, next)=>{
         cap_b_preg_18:req.body.cap_b_preg_18,
         cap_b_preg_19:req.body.cap_b_preg_19
     });
-        /*const {cap_b_preg_1, cap_b_preg_2, cap_b_preg_3, cap_b_preg_4, cap_b_preg_5, cap_b_preg_6, cap_b_preg_7, cap_b_preg_8, cap_b_preg_9, cap_b_preg_10, cap_b_preg_11, cap_b_preg_12, cap_b_preg_13, cap_b_preg_14, cap_b_preg_15, cap_b_preg_16, cap_b_preg_17, cap_b_preg_18, cap_b_preg_19}=req.body;
-    if(!cap_b_preg_1 || !cap_b_preg_2 || !cap_b_preg_3 || !cap_b_preg_4 || !cap_b_preg_5 || !cap_b_preg_6 || !cap_b_preg_7 || !cap_b_preg_8 || !cap_b_preg_9 || !cap_b_preg_10 || !cap_b_preg_11 || !cap_b_preg_12 || !cap_b_preg_13 || !cap_b_preg_14 || !cap_b_preg_15 || !cap_b_preg_16 || !cap_b_preg_17 || !cap_b_preg_18 || !cap_b_preg_19 ){
-    next(createError(400, 'falta completar preguntas'));
-    }else if(cap_b_preg_1 && cap_b_preg_2 && cap_b_preg_3 && cap_b_preg_4 && cap_b_preg_5 && cap_b_preg_6 && cap_b_preg_7 && cap_b_preg_8 && cap_b_preg_9 && cap_b_preg_10 && cap_b_preg_11 && cap_b_preg_12 && cap_b_preg_13 && cap_b_preg_14 && cap_b_preg_15 && cap_b_preg_16 && cap_b_preg_17 && cap_b_preg_18 && cap_b_preg_19 ){
-        */
         try{
             const new_B = await cap_b.save()
         res.status(201).json(new_B) 
-                        //const cap_b = new capitulo_b({cap_b_preg_1});
-            //await cap_b.save();
         }catch(ex){
             next(createError(500, 'Error trying to register the form. Try again.'))
         }
         res.json(jsonResponse(200,{
             message: 'The form has been added successfully'
         }));
-      //}
 });
 
 module.exports = router;
